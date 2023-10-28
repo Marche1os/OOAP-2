f8101c88104b39e4cd15abe63bb82537d008b24b
marche1os <kolpmark@yandex.ru>
2023-10-29 01:28:48 +0300

    ООАП-2: задание 2. Наследование с добавлением/специализацией

---
+++ src/main/kotlin/Solution2.kt
@@ -0,0 +1,21 @@
+open class View {
+    open fun show() {
+        println("Отображение view элемента")
+    }
+}
+
+// Расширение класса-родителя. Добавляем классу-родителю поведение
+class AnimationView: View() {
+
+    override fun show() {
+        super.show()
+        println("Добавление анимации отображения")
+    }
+}
+
+// Специализация класса-родителя. Специализируем класс-родитель
+class StubView : View() {
+    override fun show() {
+        // не отображаем ничего
+    }
+}
